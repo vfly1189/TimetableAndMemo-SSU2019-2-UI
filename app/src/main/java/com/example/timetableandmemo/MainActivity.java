5b718a870659caf23c4d9f98c5546c227adc7f09
catumbra <41587596+catumbra@users.noreply.github.com>
2019-11-19 15:52:29 +0900

    시간표의 첫 열 생성 방식 수정
    -기존 레이아웃xml과 style.xml로 하드코딩한 부분 제거
    -java로 시간표의 시작시간 끝시간만 주면 동적으로 첫 열 생성하는 코드 작성(MainActivity.java>fillTimetableColumn_time())

--- app/src/main/java/com/example/timetableandmemo/MainActivity.java
+++ app/src/main/java/com/example/timetableandmemo/MainActivity.java
@@ -2,20 +2,27 @@ package com.example.timetableandmemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Button directAdd;
+    LinearLayout timetableColumn_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timetableColumn_time = findViewById(R.id.timetable_column_time);
 
         directAdd = (Button)findViewById(R.id.directAdd);
         directAdd.setOnClickListener(new View.OnClickListener()
@@ -27,8 +34,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        fillTimetableColumn_time(this, timetableColumn_time, 9, 11);
     }
 
-
-
+    //시작 시간 ~ 끝나는 시간으로 첫 열 채우기
+    void fillTimetableColumn_time(Context context, LinearLayout ttLayout, int startTime, int endTime) {
+        LinearLayout.LayoutParams timeRowLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT , 0, 1);
+        for (int i = startTime; i <= endTime; i++) {
+            TextView timeRow = new TextView(context);
+            timeRow.setText(String.format("%d", i));
+            ttLayout.addView(timeRow, timeRowLayout);
+        }
+    }
 }
