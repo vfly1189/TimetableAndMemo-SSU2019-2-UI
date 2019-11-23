0c15fa1e52c70940c8d9bdc144add99cdb5ef20e
Vfly <vfly1189@naver.com>
2019-11-23 22:02:24 +0900

    DirectAdd 구조 변경

---
+++ app/src/main/java/com/example/timetableandmemo/ArrayStack.java
@@ -0,0 +1,40 @@
+package com.example.timetableandmemo;
+
+
+public class ArrayStack<T> {
+    public int top;
+    private int maxSize;
+    private T[] stackArray;
+
+    public ArrayStack(int maxSize)
+    {
+        this.maxSize = maxSize;
+        this.top = -1;
+        this.stackArray = (T[]) new Object[maxSize];
+    }
+
+    public boolean empty()
+    {
+        return (top == -1);
+    }
+
+    public boolean full()
+    {
+        return (top == maxSize-1);
+    }
+
+    public void push(T item)
+    {
+        stackArray[++top] = item;
+    }
+
+    public T top()
+    {
+        return stackArray[top];
+    }
+
+    public void pop()
+    {
+        top--;
+    }
+}
--- app/src/main/java/com/example/timetableandmemo/MainActivity.java
+++ app/src/main/java/com/example/timetableandmemo/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.timetableandmemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,4 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
         ttManager.fillTimetableColumn_time(this, timetableColumn_time, 9, 11);
     }
+
+
 }
