67171df95b308f887eb04c264623b8179d2c1b48
catumbra <41587596+catumbra@users.noreply.github.com>
2019-11-23 22:05:35 +0900

    요일별 열 안에 space 채워넣는 부분 추가

--- app/src/main/java/com/example/timetableandmemo/MainActivity.java
+++ app/src/main/java/com/example/timetableandmemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button directAdd;
     LinearLayout timetableColumn_time;
+    GridLayout[] timetableColumn_weekdays = new GridLayout[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         timetableColumn_time = findViewById(R.id.timetable_column_time);
+        timetableColumn_weekdays[0] = findViewById(R.id.timetable_column_MON);
+        timetableColumn_weekdays[1] = findViewById(R.id.timetable_column_TUE);
+        timetableColumn_weekdays[2] = findViewById(R.id.timetable_column_WED);
+        timetableColumn_weekdays[3] = findViewById(R.id.timetable_column_THU);
+        timetableColumn_weekdays[4] = findViewById(R.id.timetable_column_FRI);
 
-        TimeTableManager ttManager = new TimeTableManager();
+        TimeTableManager ttManager = new TimeTableManager("Example Title", 9, 11);
 
         directAdd = (Button)findViewById(R.id.directAdd);
         directAdd.setOnClickListener(new View.OnClickListener()
@@ -34,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ttManager.fillTimetableColumn_time(this, timetableColumn_time, 9, 11);
+        ttManager.fillTimetableColumn_time(this, timetableColumn_time);
+        for (int i = 0; i < 5; i ++) {
+            ttManager.setTimetableColumn_weekdays(this, timetableColumn_weekdays[i]);
+        }
     }
 }
--- app/src/main/java/com/example/timetableandmemo/TimeTableManager.java
+++ app/src/main/java/com/example/timetableandmemo/TimeTableManager.java
@@ -3,22 +3,53 @@ package com.example.timetableandmemo;
 import android.content.Context;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.Space;
 import android.widget.TextView;
 
 public class TimeTableManager {
 
+    private String title;
+    private int startTime;
+    private int endTime;
+
+    TimeTableManager() {
+        this.title = "DEFAULT_TABLE_TITLE";
+    }
+
+    TimeTableManager(String title) {
+        this.title = title;
+    }
+
+    TimeTableManager(String title, int startTime, int endTime) {
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
 
     //시작 시간 ~ 끝나는 시간으로 첫 열 채우기
-    void fillTimetableColumn_time(Context context, LinearLayout ttLayout, int startTime, int endTime) {
+    public void fillTimetableColumn_time(Context context, LinearLayout ttLayout) {
         LinearLayout.LayoutParams timeRowLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT , 0, 1);
-        for (int i = startTime; i <= endTime; i++) {
+        for (int i = this.startTime; i <= this.endTime; i++) {
             TextView timeRow = new TextView(context);
             timeRow.setText(String.format("%d", i));
             ttLayout.addView(timeRow, timeRowLayout);
         }
     }
 
-//    void setTimetableColumn_weekdays(Context context, GridLayout ttLayout, int startTime, int endTime) {
-//        GridLayout.LayoutParams weekdayRowLayout = new GridLayout.LayoutParams(GridLayout.LayoutParams)
-//    }
+    //시간표의 각 열을 5분단위의 Space로 채움
+    public void setTimetableColumn_weekdays(Context context, GridLayout ttLayout) {
+        int numberOfRows_5min = (this.endTime - this.startTime) * 12;
+        for (int i = 0; i < numberOfRows_5min; i++) {
+            Space minSpace = new Space(context);
+            ttLayout.addView(minSpace);
+        }
+    }
 }
