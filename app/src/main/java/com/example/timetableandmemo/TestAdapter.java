4f8de2b90cbfd96b609c6bf88d821f0752e6f022
HwangJaeHwan <58110333+HwangJaeHwan@users.noreply.github.com>
2019-12-19 16:50:41 +0900

    Revert "Revert "안드로이드에 안떠서 일단 올려볼게요요""
    This reverts commit 4d2de18e7d138e99dfe6f93d00f9fb9130b51de8.

---
+++ app/src/main/java/com/example/timetableandmemo/TestAdapter.java
@@ -0,0 +1,110 @@
+package com.example.timetableandmemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import io.realm.RealmResults;
+
+public class TestAdapter extends ArrayAdapter {
+    Context context;
+    int resId;
+    RealmResults<TestVO> mData = null;
+    Calendar tCalendar = Calendar.getInstance();
+    Calendar dCalendar = Calendar.getInstance();
+//    int tYear = tCalendar.get(Calendar.YEAR);
+//    int tMonth = tCalendar.get(Calendar.MONTH);
+//    int tDay = tCalendar.get(Calendar.DAY_OF_MONTH);
+      int d_day;
+    float t_millis = tCalendar.getTimeInMillis()/86400000;
+    float d_millis;
+
+    public TestAdapter(Context context, int resId,RealmResults<TestVO> data) {
+        super(context,resId);
+        this.context = context;
+        this.resId = resId;
+        this.mData = data;
+    }
+
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    class ViewHolder {
+        TextView subjectName;
+        TextView month;
+        TextView day;
+        TextView startHour;
+        TextView startMin;
+        TextView printDate;
+        TextView dDay;
+
+        ViewHolder(View root){
+//            subjectName = root.findViewById(R.id.subjectName_test);
+//            month = root.findViewById(R.id.month_test);
+//            day = root.findViewById(R.id.day_test);
+//            startHour = root.findViewById(R.id.startHour_test);
+//            startMin = root.findViewById(R.id.startMin_test);
+            printDate = root.findViewById(R.id.printDate);
+            dDay = root.findViewById(R.id.Dday);
+        }
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(resId, null);
+            ViewHolder holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }
+
+        ViewHolder holder = (ViewHolder)convertView.getTag();
+//        TextView subjectName = holder.subjectName;
+//        TextView month = holder.month;
+//        TextView day = holder.day;
+//        TextView startHour = holder.startHour;
+//        TextView startMin = holder.startMin;
+        TextView printDate = holder.printDate;
+        TextView dDay = holder.dDay;
+        final TestVO vo = mData.get(position);
+//        subjectName.setText(vo.getSubjectName());
+//        month.setText(vo.getMonth());
+//        day.setText(vo.getDay());
+//        startHour.setText(vo.getStartHour());
+//        startMin.setText(vo.getStartMin());
+          printDate.setText(vo.getPrintDate());
+
+        dCalendar.set(Integer.parseInt(vo.getYear()),Integer.parseInt(vo.getMonth())-1,Integer.parseInt(vo.getDay()));
+//        dCalendar.set(2019,11,3);
+        d_millis = dCalendar.getTimeInMillis()/86400000;
+
+        float count = (d_millis - t_millis);
+        d_day = (int)count;
+
+        dDay.setText("D - "+String.valueOf(d_day));
+        return convertView;
+    }
+}
